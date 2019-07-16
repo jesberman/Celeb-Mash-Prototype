f3184b632fa39ff8bd9e077412acf11d9a7fcb88
jesberman <jesberman@gmail.com>
2019-07-15 22:44:12 -0400

    added-css-file

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,51 +1,36 @@
 var celebArray = [
     {
         name: "Tom Hanks",
-        picture: "assets/images/tomHanks.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/tomHanks.jpg"
+
     },
     {
         name: "Benedict Cumberbatch",
-        picture: "assets/images/benedictCumberbatch.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/benedictCumberbatch.jpg"
     },
     {
         name: "Charlize Theron",
-        picture: "assets/images/charlizeTheron.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/charlizeTheron.jpg"
     },
     {
         name: "Evangeline Lilly",
-        picture: "assets/images/evangelineLilly.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/evangelineLilly.jpg"
     },
     {
         name: "Katee Sackhoff",
-        picture: "assets/images/kateeSackhoff.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/kateeSackhoff.jpg"
     },
     {
         name: "Robert Downey Jr.",
-        picture: "assets/images/robertDowneyJr.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/robertDowneyJr.jpg"
     },
     {
         name: "Rose Leslie",
-        picture: "assets/images/roseLeslie.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/roseLeslie.jpg"
     },
     {
         name: "Denzel Washington",
-        picture: "assets/images/denzelWashington.jpg",
-        clicks: 0,
-        score: Math.random()
+        picture: "assets/images/denzelWashington.jpg"
     },
 ];
 
@@ -53,9 +38,7 @@ var celebArrayLength = celebArray.length;
 var initialPicValue1 = Math.floor((Math.random() * celebArrayLength));
 var initialPicValue2 = Math.floor((Math.random() * celebArrayLength));
 
-
 function loadPics () {
-
     if(initialPicValue1 === initialPicValue2 && initialPicValue2 === 7){
         initialPicValue2 -= 1;
     }
@@ -73,21 +56,12 @@ function loadPics () {
 
 loadPics();
 
-
-
 function pressButton1 () {
-
-    // console.log("Array Before Button Press:");
-    // console.log(celebArray);
     console.log("Initial Pic 2 Value");
     console.log(initialPicValue2);
     console.log("Celeb Being Removed");
     console.log(celebArray[initialPicValue2].picture);
-    // alert("Button 1 Pressed");
-    // var valLeft = initialPicValue1;
-    // var valRight = initialPicValue2;
     celebArray.splice(initialPicValue2,1);
-    // console.log(valRight);
 
     celebArrayLength = celebArray.length;
     initialPicValue1 = Math.floor((Math.random() * celebArrayLength));
@@ -116,17 +90,11 @@ function pressButton1 () {
 }
 
 function pressButton2 () {
-    // console.log("Array Before Button Press:");
-    // console.log(celebArray);
     console.log("Initial Pic 1 Value");
     console.log(initialPicValue1);
     console.log("Celeb Being Removed");
     console.log(celebArray[initialPicValue1].picture);
-    // alert("Button 1 Pressed");
-    // var valLeft = initialPicValue1;
-    // var valRight = initialPicValue2;
     celebArray.splice(initialPicValue1,1);
-    // console.log(valRight);
 
     celebArrayLength = celebArray.length;
     initialPicValue1 = Math.floor((Math.random() * celebArrayLength));
@@ -152,25 +120,3 @@ function pressButton2 () {
     console.log("Array After Button Press:");
     console.log(celebArray);
 }
-
-
-// function test () {
-//     document.getElementById("test").innerHTML = celebArray[0].score;
-//     document.getElementById("test2").innerHTML = celebArray[2].score;
-// }
-
-// test();
-
-// function sortCelebs(){
-//     celebArray.sort(function(a,b){
-//         return a.score - b.score;
-//     })
-// }
-
-// sortCelebs();
-
-// function loadImage() {
-//     $("#button1").click(function(){
-//         $("picture1").addClass("intro");
-//       });
-// }
